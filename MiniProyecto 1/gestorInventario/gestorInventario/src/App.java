ac24f03c7b74a7e9d17a0827dfe3a4129e6dd26b
SantiagoAguileraA <aguileraardilasantiago13@gmail.com>
2026-08-15 16:31:41 -0500

    Mejoras de codigo

--- MiniProyecto 1/gestorInventario/gestorInventario/src/App.java
+++ MiniProyecto 1/gestorInventario/gestorInventario/src/App.java
@@ -1,3 +1,38 @@
+// import java.util.Scanner;
+
+// public class App {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int opcion = 0;
+
+//         while (opcion != 5) {
+//             Menu.mostrar();
+//             opcion = Menu.leerOpcion(sc);
+
+//             switch (opcion) {
+//                 case 1:
+//                     Operaciones.registrar(sc);
+//                     break;
+//                 case 2:
+//                     Operaciones.ver();
+//                     break;
+//                 case 3:
+//                     Operaciones.actualizarCantidad(sc);
+//                     break;
+//                 case 4:
+//                     Operaciones.verValorTotal();
+//                     break;
+//                 case 5:
+//                     System.out.println("Gracias por usar el gestor. Hasta pronto.");
+//                     break;
+//                 default:
+//                     System.out.println("Opcion no valida. Elige entre 1 y 5.");
+//             }
+//         }
+//         sc.close();
+//     }
+// }
 import java.util.Scanner;
 
 public class App {
@@ -15,7 +50,7 @@ public class App {
                     Operaciones.registrar(sc);
                     break;
                 case 2:
-                    Operaciones.ver();
+                    Operaciones.listar();
                     break;
                 case 3:
                     Operaciones.actualizarCantidad(sc);
